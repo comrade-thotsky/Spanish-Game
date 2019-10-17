7ce132e535e9a83a09e2c99dfb6b4caf9203a80a
LilKalebFTW2 <lilkalebftw2@gmail.com>
2019-10-17 09:21:24 -0400

    hj

--- Questions.js
+++ Questions.js
@@ -1,11 +1,18 @@
 function CreateQuestion(Question, ChoiceOne, ChoiceTwo, ChoiceThree, Answer){
-    // Universal Question Creator BY LOGAN
-
+    //UNIVERSAL QUESTION BUILDER MADE BY KALEB
+    document.getElementById('question').innerHTML = Question;
+    document.getElementById('ans1').innerHTML = ChoiceOne;
+    document.getElementById('ans2').innerHTML = ChoiceTwo;
+    document.getElementById('ans3').innerHTML = ChoiceThree;
+    Wrong();
 }
+
+CreateQuestion('test question', 'answer1', 'answer2', 'answer3', 3);
 
 function Wrong(){
 
 }
+
 function Right(){
     
 }
